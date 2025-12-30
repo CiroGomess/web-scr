5ebf3dd10211bac79952265e342c38eae14a130e
CiroGomess <ciro.ggj@gmail.com>
2025-12-30 11:11:34 -0300

    .

--- backend/testelogin.py
+++ backend/testelogin.py
@@ -1,12 +1,12 @@
 import asyncio
 from playwright.async_api import async_playwright
 # Troque aqui para testar o Fornecedor 3
-from controllers.fornecedores.Fornecedor7Controller import login_rmp
+from controllers.fornecedores.Fornecedor11Controller import login_dpk
 
 async def testar_acesso():
     print("🚀 Iniciando teste de login FORNECEDOR 3...")
     async with async_playwright() as p:
-        browser, context, page = await login_rmp(p)
+        browser, context, page = await login_dpk(p)
         
         if page:
             print(f"✅ Sucesso! Logado em: {page.url}")
